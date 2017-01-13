2b9c139b98564e51258f85b63bdcc5015c493e53
Javier Ros <tolemac@jros.org>
2017-01-13 15:58:29 +0100

    style: refactor

--- tolefocus/core/FocusManager.ts
+++ tolefocus/core/FocusManager.ts
@@ -4,23 +4,17 @@ export class FocusManager {
     root = new FocusGroup(null, null);
 
     focusNext(currentElement: HTMLElement) {
-        const {group, elementIndex} = this.root.locateElementRecursively(currentElement);
+        const {group} = this.root.locateElementRecursively(currentElement);
         if (group) {
             const element = group.getNextElement(currentElement);
             if (element) {
                 element.focus();
             }
-        //     const element = direction === "next" ?
-        //         this.getNextElement(group, groupIndex, elementIndex) :
-        //         this.getPriorElement(group, groupIndex, elementIndex);
-        //     if (element) {
-        //         element.focus();
-        //     }
         }
     }
 
     focusPrior(currentElement: HTMLElement) {
-        const {group, elementIndex} = this.root.locateElementRecursively(currentElement);
+        const {group} = this.root.locateElementRecursively(currentElement);
         if (group) {
             const element = group.getPriorElement(currentElement);
             if (element) {
